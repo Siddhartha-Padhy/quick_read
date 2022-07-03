bdab11be157a2243f3e97bfe9f1b7783016490b2
Siddhartha-Padhy <siddhartha6222@gmail.com>
2022-07-03 21:09:36 +0530

    first commit

---
+++ news_section/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+
+def index(request):
+    return render(request, 'home.html')
